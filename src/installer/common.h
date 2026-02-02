68df49f85a214b9c14accab1db8926725b7334ea
Juraj Kollár <mostypc7@gmail.com>
2026-02-02 14:22:36 +0100

    minor(inst): update common.h

--- src/installer/common.h
+++ src/installer/common.h
@@ -1,17 +1,20 @@
 #pragma once
 
-// ansi color codes
-#define BLUE 94
-#define RED 91
-#define YELLOW 93
-#define GREEN 92
-#define RESET 0
+// ANSI color codes
+#define BLUE    94
+#define RED     91
+#define YELLOW  93
+#define GREEN   92
+#define RESET   0
 
 int set_text_color(unsigned color);
 int clear(void);
 void enter_continue(void);
 void separator(void);
 void main_header(void);
+void installing_header(void);
+void installed_header(void);
+void install_failed(void);
 char *localization_header(void);
 char *language(void);
 char *timezone(void);
@@ -20,9 +23,12 @@ char *user_creation(void);
 char *user_password(void);
 char *root_password(void);
 char *hostname(void);
-char *propiertary_enabled(void);
-void installing_header(void);
-void installed_header(void);
-void install_failed(void);
+char *proprietary_enabled(void);
 int list_dev(void);
-int iso_to_img(char*);
+char *get_partition(const char *drive, int partnum);
+int wipe_drive(char *drive);
+int makefs(char *drive);
+int unsquash(char *drive);
+int install_grub(char *drive);
+int iso_to_img(char *path);
+
